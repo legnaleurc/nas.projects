9ba35d50fab79f599101dacdfe5c395221d1f9ef
Wei-Cheng Pan <legnaleurc@gmail.com>
2026-03-21 10:18:38 +0900

    feat: update

--- feed/scripts/feed.sh
+++ feed/scripts/feed.sh
@@ -3,7 +3,7 @@
 exec uvx \
   --quiet \
   --isolated \
-  --from "wcpan-drive-feed[inotify]" \
+  --from "wcpan-drive-feed[inotify] @ git+https://github.com/legnaleurc/wcpan.drive.feed@_backlog" \
   -- \
   wcpan.drive.feed \
     --config=/mnt/feed.yaml
