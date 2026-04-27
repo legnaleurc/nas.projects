afbf0d13ba89517fc4125a1e480a970687af2902
Wei-Cheng Pan <legnaleurc@gmail.com>
2026-04-27 23:14:46 +0900

    feat: update

--- drive/scripts/drive.sh
+++ drive/scripts/drive.sh
@@ -4,7 +4,7 @@ exec uvx \
   --quiet \
   --isolated \
   --refresh \
-  --from "wcpan-drive-synology[server]==3.0.0" \
+  --from "wcpan-drive-synology[server]==3.1.0" \
   -- \
   wcpan.drive.synology \
     --log-level=DEBUG \
